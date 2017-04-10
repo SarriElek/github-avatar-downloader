696efdb5ff0de9c34f0e75406a30a50f0619a82a
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-04-10 21:14:08 +0000

    concatenate all variables to construct the URL

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,10 @@
 // DEPENDENCIES
 var request = require('request');
 
+// CONSTANTS
+var GITHUB_USER = "SarriElek";
+var GITHUB_TOKEN = "6179f8595adbefe65f64a10b78603cd49dbf5910";
+
 // VARIABLES
 var repoOwner = "jquery";
 var repoName = "jquery";
@@ -18,5 +22,10 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 //request.get('https://api.github.com/repos/jquery/jquery/contributors')
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  //format the URL with the given variables
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
 }
+
+// CALL OUR FUNCTIION
+getRepoContributors(repoOwner, repoName, cb);
