100a0be26115cb452fb820060377e427d8778e45
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-04-10 21:05:24 +0000

    star getRepoContributors function that will get us the contributors for a given repository in github

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,22 @@
+// DEPENDENCIES
 var request = require('request');
 
+// VARIABLES
+var repoOwner = "jquery";
+var repoName = "jquery";
+
+// CALLBACK FUNCTION
+var cb = function(err, Result){
+  console.log("Errors:", err);
+  console.log("Result:", result);
+}
+
+// WELCOME
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+// Get all the contributors of the repo given
+//request.get('https://api.github.com/repos/jquery/jquery/contributors')
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
