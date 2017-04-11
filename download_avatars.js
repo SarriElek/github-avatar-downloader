eadecda6359cc43de58b8ff3b22b3f5f69f2b480
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-04-11 03:47:46 +0000

    Refactor error handling

--- download_avatars.js
+++ download_avatars.js
@@ -9,33 +9,48 @@ var downloadImageByURL = require('./http-functions');
 
 
 // VARIABLES
-var variableNum = process.argv.length;
+// var variableNum = process.argv.length;
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
+if(!(process.env.GITHUB_USER && process.env.GITHUB_TOKEN)) {
+  console.error('Please include your github credentials in the .env file');
+  process.exit(1);
+}
+
+// the command line arguments are present
+if(!(repoOwner && repoName)) {
+  console.error('Please enter two parameters: repository owner and repository name');
+  process.exit(2);
+}
+
+
 
 // CALLBACK FUNCTION
-var cb = function(err, response, body){
-  if(err){
+var cb = function(err, response, body) {
+  if(err) {
     console.error('Incorrect Credentials', err.message);
-  }else{
-     try {
-        var parsedBody = JSON.parse(body);
-      } catch (err) {
-        console.error('Failed to parse response body ', err.message);
-    }
-    if(parsedBody.message){
-      console.error('Repository not found', parsedBody.message);
-    }else{
-      parsedBody.forEach((contributor) => {
-        // create the local filepath for downloading the image
-        var filePath = `avatars/${contributor['login']}.jpg`;
-        // download the avatar images
-        downloadImageByURL(contributor['avatar_url'], filePath)
-      });
-    }
+    return;
   }
 
+  try {
+    var parsedBody = JSON.parse(body);
+  } catch (err) {
+    console.error('Failed to parse response body ', err.message);
+    return;
+  }
+
+  if(parsedBody.message){
+    console.error('Repository not found', parsedBody.message);
+    return;
+  }
+
+  parsedBody.forEach((contributor) => {
+    // create the local filepath for downloading the image
+    var filePath = `avatars/${contributor['login']}.jpg`;
+    // download the avatar images
+    downloadImageByURL(contributor['avatar_url'], filePath)
+  });
 };
 
 // WELCOME
@@ -63,18 +78,9 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 // CALL OUR FUNCTION ONLY IF EVERYTHING WE NEED IS PRESENT
 //  The .env file exists
-if (fs.existsSync('./.env')) {
     // the github credentials are pressent
-    if(process.env.GITHUB_USER && process.env.GITHUB_TOKEN){
-      // the command line arguments are present
-      if(variableNum === 4 && repoOwner && repoName){
-        getRepoContributors(repoOwner, repoName, cb);
-      }else{
-        console.error('Please enter two parameters: repository owner and repository name');
-      }
-    }else{
-      console.error('Please include your github credentials in the .env file');
-    }
-}else{
-  console.error('Please make sure that you have an .env file');
-}
+
+
+//phew, we can finally do some work here
+getRepoContributors(repoOwner, repoName, cb);
+
